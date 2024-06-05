881665a25919b5540e559ef316c21608f1797468
idev-coder <idevcoder7@gmail.com>
2024-06-06 06:18:17 +0700

    upddate

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: `127.0.0.1`,
+    host: `192.168.2.10`,
     user: `ragnarok`,
     password: `ragnarok`,
     database: `ragnarok`
